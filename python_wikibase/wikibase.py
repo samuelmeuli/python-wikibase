7ee94ceb591e1601e20678de4412746d323dad60
Samuel Meuli <me@samuelmeuli.com>
2018-11-28 10:42:09 +0100

    Add config defaults

--- python_wikibase/wikibase.py
+++ python_wikibase/wikibase.py
@@ -2,16 +2,24 @@ from wikibase_api import Wikibase as WikibaseApi
 
 from .data_model.entity import Item, Property
 
+DEFAULT_CONFIG = {
+    "api_url": "https://www.wikidata.org/w/api.php",
+    "oauth_credentials": None,
+    "login_credentials": None,
+    "is_bot": False,
+    "summary": "Modified using wikibase-api for Python",
+}
+
 
 class Wikibase:
     def __init__(
         self,
         # wikibase-api params
-        api_url=None,
-        oauth_credentials=None,
-        login_credentials=None,
-        is_bot=None,
-        summary=None,
+        api_url=DEFAULT_CONFIG["api_url"],
+        oauth_credentials=DEFAULT_CONFIG["oauth_credentials"],
+        login_credentials=DEFAULT_CONFIG["login_credentials"],
+        is_bot=DEFAULT_CONFIG["is_bot"],
+        summary=DEFAULT_CONFIG["summary"],
         config_path=None,
         # Other params
         language="en",
